1e58d20b068ca57651afb336f929fde4b4e627cb
SimonFangChicago <47611549+SimonFangChicago@users.noreply.github.com>
2019-10-07 17:44:38 -0500

    change font color
    change font color

--- app/src/main/java/com/csc472kunfang/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/csc472kunfang/temperatureconverter/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doConvert(View v)
+    {
+        EditText temperature_input_text = findViewById(R.id.temperature_input);
+
+        double input_number = Double.parseDouble(temperature_input_text.getText().toString());
+
+        //Log.d("HW1", "doConvert: " + temperature_input_text.getText().toString());
+
+        TextView temperature_output_view = findViewById(R.id.temperature_output);
+
+        double output_number = (input_number - 32)/1.8;
+
+        temperature_output_view.setText(String.format("%.1f", output_number));
     }
 }
