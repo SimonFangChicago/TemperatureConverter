8a5308dd93eab52252de4b3a593692898fc2b512
SimonFangChicago <47611549+SimonFangChicago@users.noreply.github.com>
2019-10-07 18:01:14 -0500

    Add convert feature
    Add convert feature

--- app/src/main/java/com/csc472kunfang/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/csc472kunfang/temperatureconverter/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,18 +18,78 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private boolean isF2C = true;
+
     public void doConvert(View v)
     {
         EditText temperature_input_text = findViewById(R.id.temperature_input);
 
         double input_number = Double.parseDouble(temperature_input_text.getText().toString());
 
+
+
         //Log.d("HW1", "doConvert: " + temperature_input_text.getText().toString());
 
         TextView temperature_output_view = findViewById(R.id.temperature_output);
 
-        double output_number = (input_number - 32)/1.8;
+        double output_number = 0;
 
-        temperature_output_view.setText(String.format("%.1f", output_number));
+        if(isF2C)
+        {
+            output_number = (input_number - 32)/1.8;
+        }
+        else
+        {
+            output_number = input_number*1.8 + 32;
+        }
+
+        String outputString = String.format("%.1f", output_number);
+        String inputString = String.format("%.1f", input_number);
+        temperature_output_view.setText(outputString);
+
+        AddHistory(isF2C,inputString,outputString);
+    }
+
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        TextView temperature_format_text1 = findViewById(R.id.temperature_format_text1);
+        TextView temperature_format_text2 = findViewById(R.id.temperature_format_text2);
+
+        switch(view.getId()) {
+            case R.id.radio_btn_f2c:
+                if (checked)
+
+                    temperature_format_text1.setText(R.string.f2c);
+                    temperature_format_text2.setText(R.string.c2f);
+
+                    isF2C = true;
+
+                    break;
+            case R.id.radio_btn_c2f:
+                if (checked)
+
+                    temperature_format_text1.setText(R.string.c2f);
+                    temperature_format_text2.setText(R.string.f2c);
+
+                    isF2C = false;
+
+                    break;
+        }
+    }
+
+    private void AddHistory(boolean F2CMode,String from,String to)
+    {
+        TextView temperature_format_text1 = findViewById(R.id.text_conversion_history);
+        if(F2CMode)
+        {
+            temperature_format_text1.setText(temperature_format_text1.getText().toString() + "\n" + from + " F" + "==>" + to + " C");
+        }
+        else
+        {
+            temperature_format_text1.setText(temperature_format_text1.getText().toString() + "\n" + from + " C" + "==>" + to + " F");
+        }
+
     }
 }
